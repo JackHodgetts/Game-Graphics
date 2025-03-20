95a176f24aefcd8c65b2e1dd035fc294ed0fba68
Jack Hodgetts <Jack.Hodgetts@mail.bcu.ac.uk>
2025-03-20 14:52:03 +0000

    Task one completed

--- labs/week08/cmp5359_lab_8-main/cmp5359_lab_8-main/Shading.hpp
+++ labs/week08/cmp5359_lab_8-main/cmp5359_lab_8-main/Shading.hpp
@@ -12,7 +12,7 @@ Eigen::Vector3f reflect(const Eigen::Vector3f& incoming, const Eigen::Vector3f& 
 {
 	// *** YOUR CODE HERE ***
 	// replace this with the reflected vector.
-	return Eigen::Vector3f::Zero();
+	return incoming.normalized() + 2 * (incoming.normalized().dot(normal.normalized())) * normal.normalized();
 	// *** END YOUR CODE ***
 }
 
@@ -30,16 +30,16 @@ float phongSpecularTerm(const Eigen::Vector3f& incomingLightDir, const Eigen::Ve
 {
 	// *** YOUR CODE HERE ***
 	// Find the reflected direction using the reflect function
-	Eigen::Vector3f reflectionDir = Eigen::Vector3f::Zero();
+	Eigen::Vector3f reflectionDir = reflect(incomingLightDir, normal);
 
 	// Find dot product between reflected and view directions.
-	float reflectDotNorm = 0.f;
+	float reflectDotNorm = reflectionDir.normalized().dot(viewDir.normalized());
 
 	// Make sure dot product is non-negative (if it's less than 0, set it to 0!)
-	reflectDotNorm = 0.f;
+	reflectDotNorm = __max(reflectDotNorm, 0);
 
 	// Finally, raise to specular exponent and return.
-	return 0.f;
+	return pow(reflectDotNorm, exponent);
 	// *** END YOUR CODE ***
 }
 
@@ -57,15 +57,16 @@ float blinnPhongSpecularTerm(const Eigen::Vector3f& incomingLightDir, const Eige
 {
 	// *** YOUR CODE HERE ***
 	// Find the half-vector (average of view dir and light dir)
-	Eigen::Vector3f halfVec = Eigen::Vector3f::Zero();
+	Eigen::Vector3f halfVec = ((incomingLightDir.normalized() + viewDir.normalized()) / 2).normalized();
 
 	// Find dot product of half-vector and normal.
-	float halfDotNorm = 0.f;
+	float halfDotNorm = halfVec.dot(normal.normalized());
 	
 	// Force the dot product to be non-negative (if <0, set to 0)
+	halfDotNorm = __max(halfDotNorm, 0);
 
 	//Return the dot product raised to the exponent
-	return 0.f;
+	return pow(halfDotNorm, exponent);
 	// *** END YOUR CODE ***
 }
 
--- labs/week08/cmp5359_lab_8-main/cmp5359_lab_8-main/Task1.cpp
+++ labs/week08/cmp5359_lab_8-main/cmp5359_lab_8-main/Task1.cpp
@@ -122,11 +122,11 @@ void drawTriangle(std::vector<uint8_t>& image, int width, int height,
 					// Subtask 3: Work out correct inputs for the phongSpecularTerm function inside drawTriangle, and draw an image!
 					// *** YOUR CODE HERE ***
 					// Work out the incoming light dir (from the light into the surface point).
-					Eigen::Vector3f incomingLightDir = Eigen::Vector3f::Zero();
+					Eigen::Vector3f incomingLightDir = light->getDirection(worldP);
 					// Work out the view direction (from surface point towards camera). Make sure it's normalized!
-					Eigen::Vector3f viewDir = Eigen::Vector3f::Zero();
+					Eigen::Vector3f viewDir = (camWorldPos - worldP).normalized();
 					// Find the specular term by calling phongSpecularTerm.
-					float specularTerm = 0.f;
+					float specularTerm = blinnPhongSpecularTerm(incomingLightDir, normP, viewDir, 10);
 					// *** END YOUR CODE ***
 
 					Eigen::Vector3f specularOut = specularColor * specularTerm;
